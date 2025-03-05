c4c0afe31e29bb22a79b4f6c0ac4b60e29c06cb8
ssql <167894882+ssqlone@users.noreply.github.com>
2025-03-05 13:04:05 +0400

    Update dl_base.sh

--- wazuh-dashboard/config/dl_base.sh
+++ wazuh-dashboard/config/dl_base.sh
@@ -9,13 +9,13 @@ MINOR_CURRENT=$(echo $WAZUH_CURRENT_VERSION | cut -d. -f3)
 
 ## check version to use the correct repository
 if [ "$MAJOR_BUILD" -gt "$MAJOR_CURRENT" ]; then
-  REPOSITORY="packages-dev.wazuh.com/pre-release"
+  REPOSITORY="packages-dev.wazuh.com"
 elif [ "$MAJOR_BUILD" -eq "$MAJOR_CURRENT" ]; then
   if [ "$MID_BUILD" -gt "$MID_CURRENT" ]; then
-    REPOSITORY="packages-dev.wazuh.com/pre-release"
+    REPOSITORY="packages-dev.wazuh.com"
   elif [ "$MID_BUILD" -eq "$MID_CURRENT" ]; then
     if [ "$MINOR_BUILD" -gt "$MINOR_CURRENT" ]; then
-      REPOSITORY="packages-dev.wazuh.com/pre-release"
+      REPOSITORY="packages-dev.wazuh.com"
     fi
   fi
 fi
